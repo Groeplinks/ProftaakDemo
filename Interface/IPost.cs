8618695403e6a052886e15acde549cf8030cf618
CasLeeflang <466933@student.fontys.nl>
2021-03-18 11:21:03 +0100

    Added projects storage and interface

---
+++ Interface/IPost.cs
@@ -0,0 +1,11 @@
+﻿namespace ProftaakDemo.Models
+{
+    public interface IPost
+    {
+        string Author { get; set; }
+        string Description { get; set; }
+        int Id { get; set; }
+        string Tag { get; set; }
+        string Title { get; set; }
+    }
+}
